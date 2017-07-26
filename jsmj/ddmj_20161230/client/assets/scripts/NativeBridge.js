1b793bd1c1798bb375fe2ff1f8b9e1d2b4388c1d
Sjw <sujiewen@gmail.com>
2017-07-26 16:52:20 +0800

    完善

--- jsmj/ddmj_20161230/client/assets/scripts/NativeBridge.js
+++ jsmj/ddmj_20161230/client/assets/scripts/NativeBridge.js
@@ -1,37 +1,18 @@
-cc.Class({
-    extends: cc.Component,
-
-    properties: {
-        // foo: {
-        //    default: null,      // The default value will be used only when the component attaching
-        //                           to a node for the first time
-        //    url: cc.Texture2D,  // optional, default is typeof default
-        //    serializable: true, // optional, default is true
-        //    visible: true,      // optional, default is true
-        //    displayName: 'Foo', // optional
-        //    readonly: false,    // optional, default is false
-        // },
-        // ...
-    },
-
-    // use this for initialization
-    onLoad: function () {
-
-    },
-
-    // called every frame, uncomment this function to activate update callback
-    // update: function (dt) {
-
-    // },
-});
-
-
-function NBWXCallback(data) {
-    cc.log("NBWXCallback:" + data);
+var NBWXCallback =  function(data) {
+    cc.log("NBWXCallback11:" + data);
     data = (data instanceof Object) ? data : eval("(" + data + ")");
     if (data.success == 0) {
         var u = data.userInfo;
-        setUser(u.unionId, u.name, u.iconurl, u.gender);
+        var ret = {};
+        ret.errcode = 0;
+        ret.account = u.unionId;
+        ret.sign = u.unionId;
+        cc.sys.localStorage.setItem("wx_account",u.unionId);
+        cc.sys.localStorage.setItem("wx_sign",u.unionId);
+        cc.log("ss =" +cc.vv.userMgr);
+        cc.vv.userMgr.onAuth(ret);
     }
     cc.log(data);
-}
+};
+ 
+global.NBWXCallback = NBWXCallback;
